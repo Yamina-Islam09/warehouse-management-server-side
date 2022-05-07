b57073a8740e379505ca611a81da0695dabe6bcc
Yamina-Islam09 <yamina151999@gmail.com>
2022-05-08 05:38:06 +0600

    mongodb connection setup done

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+//mongodb connection setup
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j0wnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('mobileStore').collection('item');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('hello store');
